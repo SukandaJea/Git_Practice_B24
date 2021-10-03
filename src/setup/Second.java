27f65c0ee2951ae1456b798c3e61f127e839d4f4
SukandaJea <sukanwa@icloud.com>
2021-10-03 12:24:16 -0400

    updated second file again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -3,9 +3,11 @@ package setup;
 public class Second {
 
     public  void m(){
+        System.out.println("first method");
 
     }
      public  void  m (int a){
+         System.out.println("second method");
 
      }
 
